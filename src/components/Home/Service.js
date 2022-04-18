7e843f171206b29af1d8394a122e73c07a2277ac
RubayatHassan <rubayat.roktim@gmail.com>
2022-04-18 21:21:25 +0600

    11th commit

--- src/components/Home/Service.js
+++ src/components/Home/Service.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css'
 
 const Service = ({service}) => {
     const {name, img, price, body} = service;
+    
+    const navigate = useNavigate()
+    const orderPage = () =>{
+        const path = '/order'
+        navigate(path)
+    }
     return (
         <div className='service'>
             <img className='image' src={img} alt="" />
             <h2>The Tourist guide of: {name}</h2>
             <h3>Guide Cost: {price}</h3>
             <p>Discription: {body}</p>
-            <button>Book this service: {name}</button>
+            <button onClick={orderPage}>Book this service: {name}</button>
         </div>
     );
 };
